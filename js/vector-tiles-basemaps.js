cd1fe56eaf713a9a43abcf0bea3541e4af8c5dff
Fabian Rechsteiner <fabian.rechsteiner@mail.ch>
2024-12-19 09:48:57 +0000

    create vector-tiles-basemaps.js

---
+++ js/vector-tiles-basemaps.js
@@ -0,0 +1,40 @@
+// VersaTiles Styles
+var VersaTiles_Colorful = L.vectorTileLayer('https://tiles.versatiles.org/assets/styles/colorful.json', {
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+});
+var VersaTiles_Eclipse = L.vectorTileLayer('https://tiles.versatiles.org/assets/styles/eclipse.json', {
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+});
+var VersaTiles_Graybeard = L.vectorTileLayer('https://tiles.versatiles.org/assets/styles/graybeard.json', {
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+});
+var VersaTiles_Neutrino = L.vectorTileLayer('https://tiles.versatiles.org/assets/styles/neutrino.json', {
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+});
+
+// Swisstopo Styles
+var Swisstopo_Basemap = L.vectorTileLayer('https://vectortiles.geo.admin.ch/styles/ch.swisstopo.basemap.vt/style.json', {
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+});
+var Swisstopo_LightBasemap = L.vectorTileLayer('https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json', {
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+});
+var Swisstopo_ImageryBasemap = L.vectorTileLayer('https://vectortiles.geo.admin.ch/styles/ch.swisstopo.imagerybasemap.vt/style.json', {
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+});
+
+// Basemap.world
+var Basemap_World = L.vectorTileLayer('https://sgx.geodatenzentrum.de/gdz_basemapworld_vektor/styles/bm_web_wld_col.json', {
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+});
+
+// Basemap.de Styles
+var Basemap_DE_Colored = L.vectorTileLayer('https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json', {
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+});
+var Basemap_DE_Topographic = L.vectorTileLayer('https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_top.json', {
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+});
+var Basemap_DE_Gray = L.vectorTileLayer('https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_gry.json', {
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+});
