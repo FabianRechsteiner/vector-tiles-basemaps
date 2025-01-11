0bc00cdf9ca0155080adc6645052872d4497ce45
Fabian Rechsteiner <fabian.rechsteiner@mail.ch>
2025-01-11 11:01:51 +0000

    add previewImage

--- js/vector-tiles-basemaps.js
+++ js/vector-tiles-basemaps.js
@@ -1,51 +1,76 @@
 // VersaTiles Styles
 var VersaTiles_Colorful = {
     styleUrl: 'https://tiles.versatiles.org/assets/styles/colorful.json',
-    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>',
+    previewImage: '../basemap_preview/VersaTiles_Colorful.png'
 };
 var VersaTiles_Eclipse = {
     styleUrl: 'https://tiles.versatiles.org/assets/styles/eclipse.json',
-    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>',
+    previewImage: '../basemap_preview/VersaTiles_Eclipse.png'
 };
 var VersaTiles_Graybeard = {
     styleUrl: 'https://tiles.versatiles.org/assets/styles/graybeard.json',
-    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>',
+    previewImage: '../basemap_preview/VersaTiles_Graybeard.png'
 };
 var VersaTiles_Neutrino = {
     styleUrl: 'https://tiles.versatiles.org/assets/styles/neutrino.json',
-    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>'
+    attribution: '&copy; <a href="https://versatiles.org">VersaTiles</a>',
+    previewImage: '../basemap_preview/VersaTiles_Neutrino.png'
 };
 
 // Swisstopo Styles
 var Swisstopo_Basemap = {
     styleUrl: 'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.basemap.vt/style.json',
-    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>',
+    previewImage: '../basemap_preview/Swisstopo_Basemap.png'
 };
 var Swisstopo_LightBasemap = {
     styleUrl: 'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json',
-    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>',
+    previewImage: '../basemap_preview/Swisstopo_LightBasemap.png'
 };
 var Swisstopo_ImageryBasemap = {
     styleUrl: 'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.imagerybasemap.vt/style.json',
-    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>'
+    attribution: '&copy; <a href="https://www.swisstopo.admin.ch/">swisstopo</a>',
+    previewImage: '../basemap_preview/Swisstopo_ImageryBasemap.png'
 };
 
 // Basemap.world
 var Basemap_World = {
     styleUrl: 'https://sgx.geodatenzentrum.de/gdz_basemapworld_vektor/styles/bm_web_wld_col.json',
-    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>',
+    previewImage: '../basemap_preview/Basemap_World.png'
 };
 
 // Basemap.de Styles
 var Basemap_DE_Colored = {
     styleUrl: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json',
-    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>',
+    previewImage: '../basemap_preview/Basemap_DE_Colored.png'
 };
 var Basemap_DE_Topographic = {
     styleUrl: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_top.json',
-    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>',
+    previewImage: '../basemap_preview/Basemap_DE_Topographic.png'
 };
 var Basemap_DE_Gray = {
     styleUrl: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_gry.json',
-    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>'
+    attribution: '&copy; <a href="https://www.bkg.bund.de/">Bundesamt für Kartographie und Geodäsie</a>',
+    previewImage: '../basemap_preview/Basemap_DE_Gray.png'
+};
+
+var basemaps = {
+    'VersaTiles_Colorful': VersaTiles_Colorful,
+    'VersaTiles_Eclipse': VersaTiles_Eclipse,
+    'VersaTiles_Graybeard': VersaTiles_Graybeard,
+    'VersaTiles_Neutrino': VersaTiles_Neutrino,
+    'Swisstopo_Basemap': Swisstopo_Basemap,
+    'Swisstopo_LightBasemap': Swisstopo_LightBasemap,
+    'Swisstopo_ImageryBasemap': Swisstopo_ImageryBasemap,
+    'Basemap_World': Basemap_World,
+    'Basemap_DE_Colored': Basemap_DE_Colored,
+    'Basemap_DE_Topographic': Basemap_DE_Topographic,
+    'Basemap_DE_Gray': Basemap_DE_Gray
 };
